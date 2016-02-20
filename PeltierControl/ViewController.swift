951bda33be4a97841025cf97cb945c591301cc74
rohit563 <rohit-banda@uiowa.edu>
2016-02-20 02:35:59 -0600

    UI Almost Done

--- PeltierControl/ViewController.swift
+++ PeltierControl/ViewController.swift
@@ -9,9 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var autoSwitch: UISwitch!
-    @IBOutlet weak var sliderLabel: UILabel!
-    @IBOutlet weak var sliderControl: UISlider!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()

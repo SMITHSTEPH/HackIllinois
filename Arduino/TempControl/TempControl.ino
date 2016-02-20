6744f4a1a151e98224b135ff18be74ccc6156234
Stephanie Smith <stephanie-k-smith@uiowa.edu>
2016-02-19 23:04:43 -0600

    temperature sensor code

--- Arduino/TempControl/TempControl.ino
+++ Arduino/TempControl/TempControl.ino
@@ -1,12 +1,29 @@
 
-const short tempSensor=0; //analog pin A0 for lm35dz
+const short tempPin=0; //analog pin A0 for lm35dz
+short temperature=0;
 void setup() 
 {
-  pinMode(tempSensor, INPUT);
+  pinMode(tempPin, INPUT);
+  Serial.begin(9600); //for testing
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
-
+  temperature=readTempSensor();
+  Serial.print(temperature);
+  Serial.println("* C");
+}
+/**
+ * This function reads in the analog input and converts this input
+ * to a temperature (in degrees celsius) by using this ADC formula:
+ * temperature = (reference voltage/resolution) * (analog voltage measured) * 100
+ * temperature = (analog voltage measured) * 100 * (5.00 V/1024)
+ * temperature = (analog voltage measured) * 0.48828125
+ */
+ //LM35DZ
+short readTempSensor()
+{
+  short temperature = analogRead(tempPin); //casting input to integer
+  temperature = temperature * 0.48828125;
+  return temperature;
 }
